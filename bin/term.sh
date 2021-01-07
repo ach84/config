7813f3cd69a248754cfbdc4f294b747c69eac150
ach <adrian.chomicki@gmail.com>
2021-01-08 00:08:35 +0100

    term.sh: ++xfce4

--- bin/term.sh
+++ bin/term.sh
@@ -1,7 +1,14 @@
+# no shell select - use by include
 
 run_mterm ()
 {
 	mlterm
+	exit 0
+}
+
+run_xfce4 ()
+{
+	xfce4-terminal
 	exit 0
 }
 
@@ -12,4 +19,7 @@ run_xterm ()
 }
 
 [ -x /usr/bin/mlterm ] && run_mterm
+[ -x /usr/bin/xfce4-terminal ] && run_xfce4
 [ -x /usr/bin/xterm ] && run_xterm
+
+exit 0
